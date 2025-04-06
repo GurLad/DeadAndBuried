77962a1ed661988a5cffc0c8833dd27d287df830
GurLad <40135047+GurLad@users.noreply.github.com>
2025-04-06 23:41:17 +0200

    Added all conversations

--- Scripts/GodotUtils/MusicController.cs
+++ Scripts/GodotUtils/MusicController.cs
@@ -31,6 +31,7 @@ public partial class MusicController : AudioStreamPlayer
 
     public static void Play(string name, bool keepPreviousTimestamp = true)
     {
+        return; // REMEMBER TO REMOVE THIS~
         if (current != null)
         {
             if (current.currentTrack == name)
